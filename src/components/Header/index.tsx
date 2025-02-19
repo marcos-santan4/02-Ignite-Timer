da3877ddd3ac40b80b80de677a1aaf8ae9d0ea4a
marcos-santan4 <ssmarcosantana@gmail.com>
2025-02-18 22:55:22 -0300

    feat: add new styles to head

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
 import { HeaderContainer } from './styles'
 
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img
+        src={logoIgnite}
+        alt="Dois triângulos verdes sobrepostos que representam a logo do ignite"
+      />
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <NavLink to="/" title="Timer">
+          {' '}
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
